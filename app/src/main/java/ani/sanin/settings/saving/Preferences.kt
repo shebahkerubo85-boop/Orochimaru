920f50c5ad0fffd775efdcb8038505d9c5335110
Shippun <shikamarushippun@gmail.com>
2026-08-20 08:14:42 +0300

    fix: default immersive mode on, remove banner status bar gap

--- app/src/main/java/ani/sanin/settings/saving/Preferences.kt
+++ app/src/main/java/ani/sanin/settings/saving/Preferences.kt
@@ -98,7 +98,7 @@ enum class PrefName(val data: Pref) {
     BlurBanners(Pref(Location.UI, Boolean::class, true)),
     BlurRadius(Pref(Location.UI, Float::class, 2f)),
     BlurSampling(Pref(Location.UI, Float::class, 2f)),
-    ImmersiveMode(Pref(Location.UI, Boolean::class, false)),
+    ImmersiveMode(Pref(Location.UI, Boolean::class, true)),
     SmallView(Pref(Location.UI, Boolean::class, true)),
     DefaultStartUpTab(Pref(Location.UI, Int::class, 0)),
     HomeLayout(
