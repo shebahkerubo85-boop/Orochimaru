00f72fed7bf62bcf88f559d9303251c2e4463126
Shippun <shebahkerubo85-boop@users.noreply.github.com>
2026-09-08 16:34:37 +0300

    fix: add discover avatar IDs to shouldSpin for focus spin animation

--- app/src/main/java/ani/sanin/util/FocusEffectUtil.kt
+++ app/src/main/java/ani/sanin/util/FocusEffectUtil.kt
@@ -186,6 +186,8 @@ object FocusEffectUtil {
         val id = v.id
         return id == R.id.mainCalendarContainer ||
                 id == R.id.mainUserAvatarContainer ||
+                id == R.id.discoverAvatar ||
+                id == R.id.tmdbDiscoveryAvatar ||
                 id == R.id.sheetMoviePluginArrow ||
                 id == R.id.exo_tracks
     }
